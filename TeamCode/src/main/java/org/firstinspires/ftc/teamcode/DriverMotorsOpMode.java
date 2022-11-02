26edac456aabb9758d898258be2c0d2112d2a6aa
TheRealWeeLad <42722621+TheRealWeeLad@users.noreply.github.com>
2022-11-01 19:04:02 -0500

    update drive group

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMotorsOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMotorsOpMode.java
@@ -18,7 +18,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="DRIVER CODE", group="Opmodes")
+@TeleOp(name="DRIVER CODE", group="Driving")
 public class DriverOpMode extends LinearOpMode {
 
     // Declare OpMode members.
