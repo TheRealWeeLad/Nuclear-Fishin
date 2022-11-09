3a3113860bb5fa17d17e87cdda2ef634c846a7ae
TheRealWeeLad <42722621+TheRealWeeLad@users.noreply.github.com>
2022-11-08 18:47:57 -0600

    renamed TeleOp to motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMotorsOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMotorsOpMode.java
@@ -18,7 +18,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="DRIVER CODE", group="Driving")
+@TeleOp(name="Motors", group="Driving")
 public class DriverOpMode extends LinearOpMode {
 
     // Declare OpMode members.
