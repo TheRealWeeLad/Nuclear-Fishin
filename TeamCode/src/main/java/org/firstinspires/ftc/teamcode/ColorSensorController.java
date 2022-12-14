1b11b488a04bf36cb999ed93ff3be7cd8017fcb2
TheRealWeeLad <42722621+TheRealWeeLad@users.noreply.github.com>
2022-12-13 18:52:56 -0600

    Convert ColorSensorController into instantiated class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorController.java
@@ -1,57 +1,45 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 
-@TeleOp(name="Color Sensor", group="Sensors")
-public class ColorSensorController extends LinearOpMode {
 
-  RevColorSensorV3 colorSensor;	// Hardware Device Object
-
-  @Override
-  public void runOpMode() {
-		// bPrevState and bCurrState represent the previous and current state of the button.
-		boolean bPrevState = false;
-		boolean bCurrState = false;
+public class ColorSensorController {
+	// Declare Color Sensor
+	RevColorSensorV3 colorSensor;
 	
-		// bLedOn represents the state of the LED.
-		boolean bLedOn = true;
-	
-		// get a reference to our ColorSensor object.
-		colorSensor = hardwareMap.get(RevColorSensorV3.class, "color_sensor");
-	
-		// Set the LED in the beginning
-		colorSensor.enableLed(bLedOn);
-	
-		// wait for the start button to be pressed.
-		waitForStart();
-	
-		while (opModeIsActive()) {
-	
-		  // check the status of the x button on either gamepad.
-		  bCurrState = gamepad1.x;
-	
-		  // check for button state transitions.
-		  if (bCurrState && (bCurrState != bPrevState))  {
-	
-				// button is transitioning to a pressed state. So Toggle LED
-				bLedOn = !bLedOn;
-				colorSensor.enableLed(bLedOn);
-		  }
-	
-		  // update previous state variable.
-		  bPrevState = bCurrState;
-		  
-		  // send the info back to driver station using telemetry function.
-		  telemetry.addData("LED", bLedOn ? "On" : "Off");
-		  telemetry.addData("Clear", colorSensor.alpha());
-		  telemetry.addData("Red  ", colorSensor.red());
-		  telemetry.addData("Green", colorSensor.green());
-		  telemetry.addData("Blue ", colorSensor.blue());
-	
-		  telemetry.update();
+	public class ColorInfo {
+		public int alpha;
+		public int red;
+		public int green;
+		public int blue;
+		public int[] argb;
+		
+		public ColorInfo(int a, int r, int g, int b) {
+			alpha = a;
+			red = r;
+			green = g;
+			blue = b;
+			argb = new int[] {alpha, red, green, blue};
 		}
+	}
+	
+	public ColorSensorController(RevColorSensorV3 colorSensor) {
+		this.colorSensor = colorSensor;
+	}
+	
+	public int[] getColors() {
+		// Set the LED On
+		colorSensor.enableLed(true);
+		
+		// Get Color Information
+		int alpha = colorSensor.alpha();
+		int red = colorSensor.red();
+		int green = colorSensor.green();
+		int blue = colorSensor.blue();
+		
+		ColorInfo colorInfo = new ColorInfo(alpha, red, green, blue);
+		
+		// Send back color information
+		return colorInfo.argb;
   }
 }
