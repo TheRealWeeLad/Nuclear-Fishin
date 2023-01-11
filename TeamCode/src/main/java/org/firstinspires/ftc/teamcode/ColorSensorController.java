2d4adac5b27238b6b3b306da9d5eef525940863f
TheRealWeeLad <42722621+TheRealWeeLad@users.noreply.github.com>
2023-01-10 19:21:37 -0600

    Uses getNormalizedColors() for efficiency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorController.java
@@ -1,45 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 
 public class ColorSensorController {
 	// Declare Color Sensor
 	RevColorSensorV3 colorSensor;
 	
-	public class ColorInfo {
-		public int alpha;
-		public int red;
-		public int green;
-		public int blue;
-		public int[] argb;
-		
-		public ColorInfo(int a, int r, int g, int b) {
-			alpha = a;
-			red = r;
-			green = g;
-			blue = b;
-			argb = new int[] {alpha, red, green, blue};
-		}
-	}
-	
 	public ColorSensorController(RevColorSensorV3 colorSensor) {
 		this.colorSensor = colorSensor;
 	}
 	
-	public int[] getColors() {
+	public NormalizedRGBA getColors() {
 		// Set the LED On
 		colorSensor.enableLed(true);
 		
-		// Get Color Information
-		int alpha = colorSensor.alpha();
-		int red = colorSensor.red();
-		int green = colorSensor.green();
-		int blue = colorSensor.blue();
-		
-		ColorInfo colorInfo = new ColorInfo(alpha, red, green, blue);
-		
 		// Send back color information
-		return colorInfo.argb;
+		return colorSensor.getNormalizedColors();
   }
 }
