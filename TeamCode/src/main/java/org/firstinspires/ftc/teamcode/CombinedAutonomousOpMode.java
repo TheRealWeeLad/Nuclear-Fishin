ed1138d09c7c208bd32bd5f66834aa0478ef6679
TheRealWeeLad <42722621+TheRealWeeLad@users.noreply.github.com>
2023-01-27 11:14:12 -0600

    Fixed Middle Parking And Color Detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CombinedAutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CombinedAutonomousOpMode.java
@@ -1,18 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import java.util.Collection;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
-import android.graphics.Color;
 import org.firstinspires.ftc.teamcode.ColorSensorController;
 
 import java.lang.Math;
-import java.util.concurrent.TimeUnit;
 
 @TeleOp(name="Combined Autonomous", group="Autonomous")
 public class CombinedAutonomousOpMode extends LinearOpMode {
@@ -22,6 +20,13 @@ public class CombinedAutonomousOpMode extends LinearOpMode {
 	// Declare color sensor and controller
 	RevColorSensorV3 colorSensor;
 	ColorSensorController colorSensorController;
+	
+	// Declare Motors and Servos
+	DcMotor leftDrive;
+	DcMotor rightDrive;
+	DcMotor extendDrive;
+	Servo leftServo;
+	Servo rightServo;
 	
 	// Bools for different sections of autonomous control
 	boolean lookingForColor = true;
@@ -34,13 +39,15 @@ public class CombinedAutonomousOpMode extends LinearOpMode {
 	boolean parking = false;
 	
 	// Time Milestones in ms
-	double moveToSleeveTime = 680;
-	double moveDelay = 3000;
-	double movePoleTime = 500;
-	double riseTime = 500;
-	double movePoleTimeAgain = 100;
-	double parkTimeLeft = 1300;
-	double parkTimeMiddle = 1300;
+	int moveToSleeveTime = 770;
+	int moveDelay = 1500;
+	int movePoleTime = 800;
+	int riseTime = 1000;
+	int movePoleTimeAgain = 200;
+	int reverseTime = 1000;
+	int parkTimeLeft = 1500;
+	int parkTimeMiddle = 1700;
+	int parkTimeRight = 1300;
 	
 	// Determine Parking Direction
 	enum ParkingDirection {LEFT, MIDDLE, RIGHT};
@@ -54,9 +61,12 @@ public class CombinedAutonomousOpMode extends LinearOpMode {
 		// Get Reference to Color Sensor Controller
 		colorSensorController = new ColorSensorController(colorSensor);
 		
-		// Find Motors
-		DcMotor leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-		DcMotor rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+		// Find Motors and Servos
+		leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+		rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+		extendDrive = hardwareMap.get(DcMotor.class, "extend_drive");
+		leftServo = hardwareMap.get(Servo.class, "grabber_left");
+		rightServo = hardwareMap.get(Servo.class, "grabber_right");
 		
 		// Reverse Left Motor
 		leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -70,11 +80,13 @@ public class CombinedAutonomousOpMode extends LinearOpMode {
 			
 			if (lookingForColor) {
 				// Move Forward to Sleeve
-				if (time < moveToSleeveTime * 2.5 / 7) {
+				if (time < moveToSleeveTime * 1.5 / 7) {
 					forward();
+					telemetry.addData("Moving To Sleeve Forward: ", time);
 				}
 				else if (time < moveToSleeveTime) {
 					left();
+					telemetry.addData("Moving To Sleeve Left: ", time);
 				}
 				else {
 					off();
@@ -85,6 +97,7 @@ public class CombinedAutonomousOpMode extends LinearOpMode {
 					}
 			}
 			else if (delaying) {
+				telemetry.addData("Delaying: ", time);
 				if (time > moveDelay) {
 					delaying = false;
 					checkingForColor = true;
@@ -100,68 +113,112 @@ public class CombinedAutonomousOpMode extends LinearOpMode {
 				if (max == colorsList[0]) {
 					parkingDirection = ParkingDirection.LEFT;
 				}
-				else if (max == colorsList[2]) {
-					parkingDirection = ParkingDirection.RIGHT;
-				}
 				else if (max == colorsList[1]) {
 					parkingDirection = ParkingDirection.MIDDLE;
+				}
+				else if (max == colorsList[2]) {
+					parkingDirection = ParkingDirection.RIGHT;
 				}
 				else {
 					parkingDirection = ParkingDirection.MIDDLE;
 				}
+				
+				telemetry.addData("Red: ", colors.red);
+				telemetry.addData("Green: ", colors.green);
+				telemetry.addData("Blue: ", colors.blue);
+				telemetry.addData("Dir: ", parkingDirection);
 				
 				checkingForColor = false;
 				movingToPole = true;
 				runtime.reset();
 			}
 			else if (movingToPole) {
-				if (time < movePoleTime) {
+				telemetry.addData("Dir: ", parkingDirection);
+				if (time < movePoleTime * 2 / 3) {
 					forward();
+					
+					telemetry.addData("Moving to Pole Forward: ", time);
+				}
+				else if (time < movePoleTime) {
+					right();
+					
+					telemetry.addData("Moving to Pole Right: ", time);
 				}
 				else {
+					off();
+					
 					movingToPole = false;
-					rising = true;
+					movingToPoleAgain = true;
 					runtime.reset();
 				}
 			}
 			else if (rising) {
 				if (time < riseTime) {
 					extend();
+					telemetry.addData("Rising: ", time);
 				}
 				else {
 					extendOff();
 					
 					rising = false;
-					moveToPoleAgain = true;
+					movingToPoleAgain = true;
 					runtime.reset();
 				}
 			}
 			else if (movingToPoleAgain) {
 				if (time < movePoleTimeAgain) {
 					forward();
+					
+					telemetry.addData("Pole Again: ", time);
 				}
-				
+				else {
+					off();
+					
+					// Release Cone
+					openClaw();
+					
+					movingToPoleAgain = false;
+					reversing = true;
+					runtime.reset();
+				}
+			}
+			else if (reversing) {
+				if (time < reverseTime) {
+					backward();
+					
+					telemetry.addData("Reversing: ", time);
+				}
+				else {
+					off();
+					
+					reversing = false;
+					parking = true;
+					runtime.reset();
+				}
 			}
 			else if (parking) {
-				telemetry.addData("Dir: ", parkingDirection);
-				telemetry.update();
+				parkingDirection = ParkingDirection.MIDDLE;
+				telemetry.addData("Parking ", parkingDirection);
+				telemetry.addData("Parking Time: ", time);
 				
 				switch (parkingDirection) {
 					case LEFT:
-						/* Park Left
-						parkLeft();
-						break; */
+						// Park Left
+						parkLeft(time);
+						break;
 					case RIGHT:
 						/* Park Right
-						parkRight();
-						break;*/
+						parkRight(time);
+						break; */
 					default:
 					case MIDDLE:
 						// Park Middle
-						parkMiddle();
+						parkMiddle(time);
 						break;
 				}
 			}
+			
+			telemetry.update();
 		}
 	}
 	
@@ -182,7 +239,7 @@ public class CombinedAutonomousOpMode extends LinearOpMode {
 	
 	void right() {
 		leftDrive.setPower(0.7);
-		rightDriver.setPower(0.3);
+		rightDrive.setPower(0.3);
 	}
 	
 	void off() {
@@ -202,30 +259,40 @@ public class CombinedAutonomousOpMode extends LinearOpMode {
 		extendDrive.setPower(0);
 	}
 	
-	void parkLeft() {
-		if (time < parkTimeLeft / 2) {
+	void openClaw() {
+		leftServo.setPosition(0.6);
+		rightServo.setPosition(0.4);
+	}
+	
+	void parkLeft(double time) {
+		if (time < parkTimeLeft / 3) {
+			left();
+		}
+		else if (time < parkTimeLeft) {
+			right();
+		}
+		else {
+			off();
+			parking = false;
+		}
+	}
+	
+	void parkRight(double time) {
+		if (time < parkTimeRight / 2) {
 			// Go Straight
 			forward();
 		}
-		else if (time < parkTimeLeft) {
+		else if (time < parkTimeRight) {
 			// Move Back to the Right
 			right();
 		}
 		else {
+			off();
 			parking = false;
 		}
 	}
 	
-	void parkRight() {
-		if (time < parkTimeMiddle) {
-			
-		}
-		else {
-			parking = false;
-		}
-	}
-	
-	void parkMiddle() {
+	void parkMiddle(double time) {
 		if (time < parkTimeMiddle * 2 / 3) {
 			// Turn back to the right
 			right();
